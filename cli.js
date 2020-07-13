2f590064ecd85fb160073e113721a075175442d4
lvshuangyong <1721446288@qq.com>
2020-07-13 20:44:45 +0800

    修改用户不输入ç显示bug

--- cli.js
+++ cli.js
@@ -1,30 +1,31 @@
 #!/usr/bin/env node
-const { program } = require('commander');
+const {program} = require('commander');
 const api = require('./index.js')
 const pak = require('./package.json')
-if (process.argv.length === 2) {
-  api.showAll()
-} else {
-  program
+program
     .option('add <task>', '添加一个任务')
-  program
+program
     .version(pak.version)
-  program
+program
     .command('add')
     .description('add a task')
     .action((source, destination) => {
-      if (!destination) { return console.log('请按照格式输入-add <任务名称>') }
-      let words = destination.join(' ')
-      api.add(words).then(() => console.log('添加成功'), () => console.log('添加失败'))
+        if (!destination) {
+            return console.log('请按照格式输入-add <任务名称>')
+        }
+        let words = destination.join(' ')
+        api.add(words).then(() => console.log('添加成功'), () => console.log('添加失败'))
     });
-  program
+program
     .command('clear')
     .description('clear all tasks')
     .action((source, destination) => {
-      api.clear().then(() => console.log('清除成功'), () => console.log('清除失败'))
+        api.clear().then(() => console.log('清除成功'), () => console.log('清除失败'))
     });
-  program.parse(process.argv);
+if (process.argv.length === 2) {
+    api.showAll()
 }
+program.parse(process.argv);
 
 
 
